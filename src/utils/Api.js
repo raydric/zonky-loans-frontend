e69f33ab143baa3512d8a105c65c0173bd551a19
Petr Pospisil <petr.pospisil1988@gmail.com>
2019-07-04 22:44:38 +0200

    Add user-agent

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -3,5 +3,8 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.zonky.cz/',
     timeout: 1000,
-    headers: {'X-Size': 100}
+    headers: {
+        'X-Size': 100,
+        'User-Agent': 'AverageLoans/1.0 https://github.com/raydric/zonky-loans-frontend'
+    }
 });
